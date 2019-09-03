8fa190703f866e2270133b5462db51b039e86dfc
Zorex Salvo <zorexsalvo@gmail.com>
2019-09-03 17:31:15 +0800

    Implement create payment

--- paymongo/api_resources/payment.py
+++ paymongo/api_resources/payment.py
@@ -1,4 +1,7 @@
+import requests
+
 from dotmap import DotMap 
+from paymongo import api_base
 
 class Payment(DotMap):
     """Payment response object."""
@@ -10,4 +13,21 @@ class PaymentService:
 
     @classmethod
     def create(cls, data, api_key=None):
-        pass
+        """
+        To charge a payment source, you must create a Payment object.
+        """
+
+        url = api_base + '/v1/payments'
+        response = requests.post(url, json={
+            'data': {
+                'attributes': data
+            }
+        }, auth=(api_key, ''))
+
+        if response.status_code not in [200, 201]:
+            # Create and raise paymongo exception
+            return Payment(response.json())
+
+        if isinstance(response.json(), dict):
+            data = response.json().get('data')
+            return Payment(data)
--- tests/test_paymongo_payment.py
+++ tests/test_paymongo_payment.py
@@ -1,5 +1,40 @@
+import os
 import paymongo
 
 
 def test_exec_create_payment():
     token = paymongo.Payment.create(None)
+
+
+def test_create_payment():
+    """
+    Create a payment.
+    """
+
+    pk = os.getenv('PUBLIC_KEY')
+    sk = os.getenv('SECRET_KEY')
+    token = paymongo.Token.create(card={
+        'number': '4242424242424242',
+        'exp_month': 12,
+        'exp_year': 22,
+        'cvc': '212'
+    },
+                                  api_key=pk)
+
+    source = {'id': token.id, 'type': 'token'}
+
+    data = {
+        'amount': 10000,
+        'currency': 'PHP',
+        'description': '',
+        'statement_descriptor': '',
+        'source': source
+    }
+
+    payment = paymongo.Payment.create(data, sk)
+
+    assert payment.type == 'payment'
+    assert payment.id is not None
+    assert payment.attributes.amount == data['amount']
+    assert payment.attributes.fee is not None
+    assert payment.relationships.source.data == source
