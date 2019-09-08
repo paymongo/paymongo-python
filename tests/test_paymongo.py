4086be4ecb3dc3f1d275af42ca7eb9b17e594faa
Zorex Salvo <zorexsalvo@gmail.com>
2019-09-08 20:56:06 +0800

    Add assertion for api_key global setting

--- tests/test_paymongo.py
+++ tests/test_paymongo.py
@@ -1,5 +1,7 @@
-from paymongo import __version__
 import paymongo
+import os
+
+from paymongo import __version__
 from paymongo.api_resources import Token, Payment
 
 
@@ -19,3 +21,7 @@ def test_initialize_paymongo_token():
 def test_initialize_paymongo_payment():
     payment = paymongo.Payment()
     assert isinstance(payment, Payment) == True
+
+def test_allow_global_setting_of_api_key():
+    paymongo.api_key = os.getenv('SECRET_KEY')
+    assert paymongo.api_key is not None
