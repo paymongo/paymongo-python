a6d8adc331da8382baef5771080eba761376771d
traqy-paymongo <traqy@paymongo.com>
2023-02-08 14:05:57 +0800

    Use api_resouce

--- paymongo/api_resources/entities/payment_method_entity.py
+++ paymongo/api_resources/entities/payment_method_entity.py
@@ -1,17 +1,14 @@
 from paymongo import BaseEntity
 
 class PaymentMethodEntity(BaseEntity):
-  def __init__(self, resource):
+  def __init__(self, api_resource):
     BaseEntity.__init__(self)
 
-    data = resource['data']
-    attributes = data['attributes']
-
-    self.id = data['id']
-    self.billing = attributes['billing']
-    self.details = attributes['details']
-    self.livemode = attributes['livemode']
-    self.metadata = attributes['metadata']
-    self.type = attributes['type']
-    self.created_at = attributes['created_at']
-    self.updated_at = attributes['updated_at']
+    self.id = api_resource.id
+    self.billing = api_resource.attributes['billing']
+    self.details = api_resource.attributes['details']
+    self.livemode = api_resource.attributes['livemode']
+    self.metadata = api_resource.attributes['metadata']
+    self.type = api_resource.attributes['type']
+    self.created_at = api_resource.attributes['created_at']
+    self.updated_at = api_resource.attributes['updated_at']
