38540f0d30b1d2d6bf8ff8272ada3ff8ad3b5969
Alelie Pangilinan <alelie@paymongo.com>
2023-02-08 14:20:05 +0800

    Use api resource

--- paymongo/api_resources/entities/refund_entity.py
+++ paymongo/api_resources/entities/refund_entity.py
@@ -1,24 +1,21 @@
 from paymongo import BaseEntity
 
 class RefundEntity(BaseEntity):
-  def __init__(self, resource):
+  def __init__(self, api_resource):
     BaseEntity.__init__(self)
 
-    data = resource['data']
-    attributes = data['attributes']
-
-    self.id = data['id']
-    self.amount = attributes['amount']
-    self.balance_transaction_id = attributes['balance_transaction_id']
-    self.currency = attributes['currency']
-    self.livemode = attributes['livemode']
-    self.metadata = attributes['metadata']
-    self.notes = attributes['notes']
-    self.payment_id = attributes['payment_id']
-    self.payout_id = attributes['payout_id']
-    self.reason = attributes['reason']
-    self.status = attributes['status']
-    self.available_at = attributes['available_at']
-    self.created_at = attributes['created_at']
-    self.refunded_at = attributes['refunded_at']
-    self.updated_at = attributes['updated_at']
+    self.id = api_resource.id
+    self.amount = api_resource.attributes['amount']
+    self.balance_transaction_id = api_resource.attributes['balance_transaction_id']
+    self.currency = api_resource.attributes['currency']
+    self.livemode = api_resource.attributes['livemode']
+    self.metadata = api_resource.attributes['metadata']
+    self.notes = api_resource.attributes['notes']
+    self.payment_id = api_resource.attributes['payment_id']
+    self.payout_id = api_resource.attributes['payout_id']
+    self.reason = api_resource.attributes['reason']
+    self.status = api_resource.attributes['status']
+    self.available_at = api_resource.attributes['available_at']
+    self.created_at = api_resource.attributes['created_at']
+    self.refunded_at = api_resource.attributes['refunded_at']
+    self.updated_at = api_resource.attributes['updated_at']
