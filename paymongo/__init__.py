3403985c86b8542116dfd3d0d4e640ff9d42efdf
cemanalo <carloeugene@gmail.com>
2023-06-29 11:23:13 +0800

    Fixed circular dependencies

--- paymongo/__init__.py
+++ paymongo/__init__.py
@@ -13,7 +13,6 @@ from paymongo.api_resources.paymongo_client import PaymongoClient
 from paymongo.api_resources.entities.base_entity import BaseEntity
 from paymongo.api_resources.entities.billing_address_entity import BillingAddressEntity
 from paymongo.api_resources.entities.billing_entity import BillingEntity
-from paymongo.api_resources.entities.checkout_session_entity import CheckoutSessionEntity
 from paymongo.api_resources.entities.customer_entity import CustomerEntity
 from paymongo.api_resources.entities.event_entity import EventEntity
 from paymongo.api_resources.entities.line_item_entity import LineItemEntity
@@ -25,6 +24,7 @@ from paymongo.api_resources.entities.payment_intent_entity import PaymentIntentE
 from paymongo.api_resources.entities.payment_method_entity import PaymentMethodEntity
 from paymongo.api_resources.entities.refund_entity import RefundEntity
 from paymongo.api_resources.entities.webhook_entity import WebhookEntity
+from paymongo.api_resources.entities.checkout_session_entity import CheckoutSessionEntity
 
 from paymongo.api_resources.services.base_service import BaseService
 from paymongo.api_resources.services.checkout_session import CheckoutSession
